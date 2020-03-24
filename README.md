bf52f964d79953435fc053f6eafa4f562366d587
leoreisdias <leoanrdord99@gmail.com>
2020-03-24 18:51:31 -0300

    Readme Style

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # primeirosGraficos-Java
-Beginning some Java Graphics concepts for the area of Game Development 
-(Começando com alguns conceitos gráficos em Java para Desenvolvimento de Jogos)
+<h2>Beginning some Java Graphics concepts for the area of Game Development 
+(Começando com alguns conceitos gráficos em Java para Desenvolvimento de Jogos)</h2>
+<hr />
+<h3>Utilizando de Canvas e JFrames.</h3>
 
-Utilizando de Canvas e JFrames.
-
-Com Canvas defini-se propriedades para a janela do jogo, tal como largura e altura, e para melhor controle uma variavel SCALE auxiliará na manipulação das dimensões.
+<p style="background-color: gray;">Com Canvas defini-se propriedades para a janela do jogo, tal como largura e altura, e para melhor controle uma variavel SCALE auxiliará na manipulação das dimensões.</p>
 
 JFrame trabalha com essas propriedades e pode gerenciar funcionalidades básicas da janela como posição, comportamento ao fechar e abrir, entre outros.
--- graficos/src/graficos/Game.java
+++ graficos/src/graficos/Game.java
@@ -8,12 +8,19 @@ import javax.swing.JFrame;
 public class Game extends Canvas implements Runnable {
 
 	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
 	private final int WIDTH = 160;
 	private final int HEIGHT = 120;
 	private final int SCALE = 3;
 
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+
+	}
+
+	public void initFrame() {
 		frame = new JFrame("Janela Inicial");
 		frame.add(this);
 		frame.setResizable(false);
@@ -21,14 +28,26 @@ public class Game extends Canvas implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
 
 	}
 
 	public static void main(String args[]) {
 		Game game = new Game();
+		game.start();
 	}
 
 	public void run() {
+		while (isRunning) {
 
+		}
 	}
 }
