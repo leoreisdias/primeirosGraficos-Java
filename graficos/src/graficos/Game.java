484ca5fca622d1ba2396b90b998c8e07d75f56c5
leoreisdias <leoanrdord99@gmail.com>
2020-03-24 19:25:18 -0300

    FPS in loop
    Nano segundos para ter melhor precisão na contagem
    60 Frames per Second escolhidos
    A cada NS segundos será contado um Frame
    Delta armazena o tempo em nano segundos decorrido até alcançar o NS
    Delta vale 1 ao alcançar o tempo de contage e o frame incrementa
    Delta decrementa para iniciar reiniciar o processo
    
    A cada 1 segundo é exibido em tela os frames objetivados.

--- graficos/src/graficos/Game.java
+++ graficos/src/graficos/Game.java
@@ -45,9 +45,37 @@ public class Game extends Canvas implements Runnable {
 		game.start();
 	}
 
-	public void run() {
-		while (isRunning) {
+	public void tick() {
 
+	}
+
+	public void render() {
+
+	}
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			if (delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
 		}
 	}
 }
